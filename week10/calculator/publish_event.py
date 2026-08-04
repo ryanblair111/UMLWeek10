6278835881210f3b96b887abead6dfc1727aca9e
ryanblair111@gmail.com <ryan@Ryans-MacBook-Pro.local>
2026-08-03 21:03:54 -0400

    Updates to publish_event.py

--- week10/calculator/publish_event.py
+++ week10/calculator/publish_event.py
@@ -20,7 +20,7 @@ event = {
     "event": "ImagePushed",
     "image": "calculator",
     "registry": "localhost:5001",
-    # TODO: add the version, for example  "version": os.environ["VERSION"]
+    "version": os.environ["VERSION"]
 }
 producer.send(os.environ.get("TOPIC", "ci.images"), event).get(timeout=10)
 producer.flush()
